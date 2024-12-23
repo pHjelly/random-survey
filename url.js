3a2d43f767a121e784160aa57566806a19107c9f
pHjelly <phjellyfish0324@gmail.com>
2024-12-23 13:27:03 +0800

    Create url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://docs.google.com/forms/d/e/1FAIpQLScbqxl66jlP8gIs-j9TCjTEs8t_cvrsYLfBwOse0G58f6kd6Q/viewform?usp=header',
+	'https://docs.google.com/forms/d/e/1FAIpQLSfVbVU7YGv3XlRhF2xRp6CN7U0noKRmjXLBCVs0eivx6xfG5Q/viewform?usp=header',
 ];
